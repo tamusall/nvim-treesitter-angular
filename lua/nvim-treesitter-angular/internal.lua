f6b1c3c6a6e16299268a8af759b5279535bd0be1
Travis <tamusall@gmail.com>
2022-09-25 17:00:45 -0400

    updating `internal.lua` to reflect updated gen files.

--- lua/nvim-treesitter-angular/internal.lua
+++ lua/nvim-treesitter-angular/internal.lua
@@ -1,21 +1,21 @@
-local parsers = require "nvim-treesitter.parsers"
-local installer = require "nvim-treesitter.install"
+local parsers = require("nvim-treesitter.parsers")
+local installer = require("nvim-treesitter.install")
 
 local M = {}
 
 function M.init()
-  parsers.get_parser_configs().angular = {
-    install_info = {
-      url = "https://github.com/steelsojka/tree-sitter-angular",
-      files = { "src/parser.c" },
-      branch = "main"
-    },
-    maintainers = {"@steelsojka"}
-  }
+	parsers.get_parser_configs().angular = {
+		install_info = {
+			url = "https://github.com/tamusall/tree-sitter-angular",
+			files = { "src/parser.c" },
+			branch = "main",
+		},
+		maintainers = { "@tamusall" },
+	}
 
-  if not parsers.has_parser("angular") then
-    installer.update("angular")
-  end
+	if not parsers.has_parser("angular") then
+		installer.update("angular")
+	end
 end
 
 return M
